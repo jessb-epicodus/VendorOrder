573ef31501711c0d6d0160f4c39823fcc391f833
jb <jessb.epicodus@gmail.com>
2022-03-04 10:07:05 -0800

    good fail: order details

--- VendorOrder.Tests/ModelTests/OrderTests.cs
+++ VendorOrder.Tests/ModelTests/OrderTests.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System;
 using VendorOrder.Models;
 // title(eventually replace with ID), description, price, date
-// create instance of Order
+// create instance of Order  X
 // get properties, return properties
 // GetAll, return list, **IDispose
 // GetAll, retun list of orders 
@@ -15,10 +15,24 @@ namespace VendorOrder.Tests {
     [TestMethod]
     public void OrderConstructor_CreateInstanceOfOrder_Order() {  
       // arrange
-      Order testOrder = new Order();
+      Order testOrder = new Order("title", "description", 100, "date");
       // act
       // assert
       Assert.AreEqual(typeof(Order), testOrder.GetType());
+    }
+    [TestMethod]
+    public void OrderDetails_ReturnsOrderDetail_OrderDetail() {
+      //Arrange
+      string title = "title";
+      // string description = "description";
+      // int price = 100;
+      // string date = "date";
+      Order testOrder = new Order("title", "description", 100, "date");
+      //Act
+      string result = testOrder.Title;
+      //Assert
+      Assert.AreEqual(title, "");
+
     }
   }
 }
--- VendorOrder/Models/Order.cs
+++ VendorOrder/Models/Order.cs
@@ -2,6 +2,16 @@ using System.Collections.Generic;
 // title(eventually replace with ID), description, price, date
 namespace VendorOrder.Models {
   public class Order {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public string Date { get; set; }
 
+    public Order(string title, string description, int price, string date) {
+      Title = title;
+      Description = description;
+      Price = price;
+      Date = date;
+    }
   }
 }
